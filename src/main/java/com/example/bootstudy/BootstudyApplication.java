a95c14cac0bf023ac6288f42efb849474de95723
MinKyung <omi7894@naver.com>
2023-10-14 14:56:03 +0900

    initial commit

---
+++ src/main/java/com/example/bootstudy/BootstudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstudyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/bootstudy/controller/GetController.java
@@ -0,0 +1,62 @@
+package com.example.bootstudy.controller;
+
+import com.example.bootstudy.dto.MemberDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Member;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello(){
+        return "HelloWorld";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "Flature";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request1?name=flature&email=thinkground.flature@gmail.com&organization=thinkground
+    @GetMapping(value = "request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        return name + " " + email + " " + organization;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request2?key1=value1&key2=value2
+    @GetMapping(value = "request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // http://localhost:8080/api/v1/get-api/request3?name=flature&email=thinkground.flature@gmail.com&organization=thinkground
+    @GetMapping(value = "request3")
+    public String getRequestParam3(MemberDTO memberDTO){
+        //return memberDTO.getName() + " " + memberDTO.getEmail() + " " + memberDTO.getOrganization();
+        return memberDTO.toString();
+    }
+
+
+}
